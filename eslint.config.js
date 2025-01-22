4723f7191ea968f4fb37c8d7d2ebb25d37a058cc
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-01-22 12:31:32 -0500

    Updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,21 @@
-import tjwJest from './index.js';
+import pluginJest from 'eslint-plugin-jest';
+
+import pluginTjwJest from './index.js';
 
 export default [
-  ...tjwJest
+  pluginJest.configs['flat/recommended'],
+  pluginTjwJest.configs.recommended,
+  // Your project rules/settings
+  {
+    // If you are using Vitest
+    languageOptions: {
+      globals: {
+        vi: true
+      }
+    },
+    rules: {
+      // If this is not turned off, linting throws because it can't find 'jest' install
+      'jest/no-deprecated-functions': 'off'
+    }
+  }
 ];
